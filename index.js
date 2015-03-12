d7acbcca7b97409483f590ada7fbf672ac88e371
Chinmay Pendharkar <notthetup@gmail.com>
2015-03-12 13:12:43 +0800

    reorg for gh page deployment

--- index.js
+++ index.js
@@ -1,99 +1,18 @@
 "use strict";
 
-var WebAudioLoader = require('webaudioloader');
-var encoder = require('encode-wav');
+var resampler = require('./lib/resampler.js');
 
-// WebAudio Shim.
-window.OfflineAudioContext = window.OfflineAudioContext || window.webkitOfflineAudioContext;
-window.AudioContext = window.AudioContext || window.webkitAudioContext;
-
-var audioContext = new AudioContext();
-
-var wal = new WebAudioLoader({
-	context : audioContext,
-	cache : false
+window.addEventListener('load', function(){
+	resampler('https://dl.dropboxusercontent.com/u/77191118/sounds/Hit5.mp3',192000, function(event){
+			event.getFile(function(fileEvent){
+				console.log(fileEvent);
+				var a = document.createElement("a");
+				document.body.appendChild(a);
+				a.style = "display: none";
+				a.href = fileEvent;
+				a.download = "resampled.wav";
+				a.click();
+				window.URL.revokeObjectURL(fileEvent);
+			});
+	});
 });
-
-/*
-* resampler = require('resampler');
-* resampler(file/URL/AudioBuffer, samplerRate, oncomplete) -> Promise
-* event.getFile();
-* event.getAudioBuffer();
-*/
-
-
-/*
-*
-*/
-function resampler(file, targetSampleRate, oncomplete){
-
-	if (!file && !targetSampleRate){
-		console.error('Error: First two arguments (audio file & target sample rate) are mandatory');
-		return; // return Rejected Promise
-	}
-
-	var inputType = Object.prototype.toString.call( file );
-	if (inputType !== '[object String]' && inputType !== '[object File]' &&
-		inputType !== '[object Blob]' && inputType !== '[object AudioBuffer]'){
-		console.error('Error: First argument should be either a File, URL or AudioBuffer');
-		return; // return Rejected Promise
-	}
-
-	if(typeof targetSampleRate !== 'number' || targetSampleRate > 192000 || targetSampleRate < 3000){
-		console.error('Error: Second argument should be a numeric sample rate between 3000 and 192000');
-		return; // return Rejected Promise
-	}
-
-	if (inputType !== '[object AudioBuffer]'){
-		console.log('Loading/decoding file', file);
-		wal.load(file, {onload: fileDecodedCallback});
-	} else{
-		fileDecodedCallback(null, file);
-	}
-
-	function fileDecodedCallback(err, audioBuffer){
-		if (err){
-			console.error(err);
-			return; // return Rejected Promise
-		}
-
-		console.log('Load/decod complete');
-
-		var numCh_ = audioBuffer.numberOfChannels;
-		var numFrames_ = audioBuffer.length*targetSampleRate/audioBuffer.sampleRate;
-
-		var offlineContext_ = new OfflineAudioContext(numCh_, numFrames_, targetSampleRate);
-		var bufferSource_ = offlineContext_.createBufferSource();
-		bufferSource_.buffer = audioBuffer;
-
-		offlineContext_.oncomplete = function(event){
-			var resampeledBuffer = event.renderedBuffer;
-			console.log('Done Rendering');
-			if (typeof oncomplete === 'function'){
-				oncomplete({
-					getAudioBuffer: function(){
-						return resampeledBuffer;
-					},
-					getFile : function (fileCallback){
-						encoder.encodeWAV([resampeledBuffer.getChannelData(0), resampeledBuffer.getChannelData(1)],
-							resampeledBuffer.sampleRate,
-							function(blob) {
-								console.log('wav encoding complete: ', blob );
-								if (blob) {
-									fileCallback(URL.createObjectURL(blob));
-								}
-							});
-					}
-				});
-			}
-		};
-
-		console.log('Starting Offline Rendering');
-		bufferSource_.connect(offlineContext_.destination);
-		bufferSource_.start(0);
-		offlineContext_.startRendering();
-	}
-}
-
-
-module.exports = resampler;
