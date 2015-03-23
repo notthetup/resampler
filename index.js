15c5870d966a081c5b9092cba008c4261608d28e
Chinmay Pendharkar <notthetup@gmail.com>
2015-03-23 14:10:54 +0800

    added spinner

--- index.js
+++ index.js
@@ -8,15 +8,20 @@ window.addEventListener('load', function(){
 	var fSelectOption = document.getElementById('freqSelect');
 	var messageBox = document.getElementById('message');
 	var input = document.getElementById('input');
+	var note = document.getElementById('note');
+	var spinner = document.getElementById('spinner');
 	messageBox.addEventListener('click', function (){
 		input.click();
 	});
 
 	input.addEventListener('change', function(evt){
 		var chosenFile = evt.target.files[0];
-		var chosenSampleRate = parseInt(fSelectOption.selectedOptions[0].value);
-		console.log(chosenFile,chosenSampleRate);
-		resampleFile(chosenFile,chosenSampleRate);
+		if (chosenFile){
+			var chosenSampleRate = parseInt(fSelectOption.selectedOptions[0].value);
+			console.log(chosenFile,chosenSampleRate);
+			resampleFile(chosenFile,chosenSampleRate);
+		}
+
 	});
 
 	dragDrop('#dropzone', function (files) {
@@ -27,9 +32,16 @@ window.addEventListener('load', function(){
 	});
 
 	function resampleFile (file, targetSampleRate){
+		// note.messageBox.
+		note.style.display = "none";
+		spinner.style.display = "inherit";
+		input.disabled = true;
 		resampler(file, targetSampleRate, function(event){
 			event.getFile(function(fileEvent){
 				console.log(fileEvent);
+				spinner.style.display = "none";
+				note.style.display = "inherit";
+				input.disabled = false;
 				var a = document.createElement("a");
 				document.body.appendChild(a);
 				a.style = "display: none";
